26f7c5f536c117d178749ae9a65fe266e2bd6d86
Adosin <Adosin.Lo@gmail.com>
2018-07-20 18:14:22 +0800

    add calc avg length of reviews

--- read3.py
+++ read3.py
@@ -8,8 +8,15 @@ with open('reviews.txt', 'r') as f:
 			print('第', len(data), '筆數')  
 print('總共留言筆數: ', len(data))  
 print('---------------')
-print('第一筆留言內容:')
-print(data[0])
-print('---------------')
-print('第二筆留言內容:')
-print(data[1]) 
+# print('第一筆留言內容:')
+# print(data[0])
+# print('---------------')
+# print('第二筆留言內容:')
+# print(data[1]) 
+
+
+sum_len = 0 # 計算留言平均長度
+for d in data:
+	sum_len = sum_len + len(d) 
+
+print('留言的平均長度是', sum_len/len(data)) 
